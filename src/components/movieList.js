3bc494fee472cdcdfdba1d617e2de6f4a55984c7
Shubham Singh <93003514+shubham211195@users.noreply.github.com>
2022-03-21 21:45:17 +0530

    Update movieList.js

--- src/components/movieList.js
+++ src/components/movieList.js
@@ -8,6 +8,7 @@ const MovieList =(props)=>{
             {props.movies.map((movie,index)=>
                 <div className="forHover">
                     <img src={movie.Poster} alt="poster"></img>
+                       <h4>{movie.Title}</h4>
                 </div>
             )}
         </>
